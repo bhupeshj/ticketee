bcbcc7f9ce09b48d7d7d15cecc4e4d5ee79e4717
bhupesh jaywant <bhupeshj@gmail.com>
2014-02-01 21:11:30 -0800

    Implemented signing in

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ resources :projects do
 end
 
 resources :users
+
+get "/signin", to: "sessions#new"
+post "/signin", to: "sessions#create"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
